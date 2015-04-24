1704a7630b218449b534bdcb3cfc0b6ff605d2f2
John Moons <j.moons@realdigitalmedia.com>
2015-04-24 18:06:02 -0400

    1) Added benchmarking 2) Added print method 3) OnePass is working and definitely the way to roll for arrays with more than 5 values

--- minimum_scalar_product.rb
+++ minimum_scalar_product.rb
@@ -1,25 +1,40 @@
+require 'benchmark'
+NUMBER_OF_ITERATIONS = 1
+
 class MSP_OnePass
   def initialize(vector1, vector2)
     @vector1      = vector1
     @vector2      = vector2
     @smallest_sum = nil
+    @largest_sum  = nil
   end
 
   def run
-    vector1_sorted          = @vector1.sort
-    vector2_sorted_reversed = @vector2.sort.reverse
-    calculate_sum_product(vector1_sorted, vector2_sorted_reversed)
-    puts "DONE (ONE PASS): #{@smallest_sum}"
+    vector1_sorted = @vector1.sort
+    vector2_sorted = @vector2.sort
+
+    @smallest_sum = calculate_sum_product(vector1_sorted, vector2_sorted.reverse)
+    @largest_sum  = calculate_sum_product(vector1_sorted, vector2_sorted)
+
+    self
+  end
+
+  def print_results
+    puts "DONE (OnePass SM): #{@smallest_sum}"
+    puts "DONE (OnePass LG): #{@largest_sum}"
   end
 
   private
+
   def calculate_sum_product(vector1, vector2)
     sum = 0
     (0 ... vector1.length).each do |current_index|
       sum += (vector1[current_index] * vector2[current_index])
     end
-    @smallest_sum = sum
+
+    sum
   end
+
 end
 
 class MinimumScalarProduct
@@ -29,10 +44,16 @@ class MinimumScalarProduct
     @smallest_sum = nil
     @largest_sum  = nil
   end
+
   def run
     @vector1.permutation.each do |vector1_permutation|
       test_permutation(vector1_permutation)
     end
+
+    self
+  end
+
+  def print_results
     puts "DONE (Small): #{@smallest_sum.inspect}"
     puts "DONE (Large): #{@largest_sum.inspect}"
   end
@@ -59,16 +80,50 @@ class MinimumScalarProduct
   end
 end
 
-MSP_OnePass.new([1,3,-5], [-2,4,1]).run
-MinimumScalarProduct.new([1,3,-5], [-2,4,1]).run
 
-MSP_OnePass.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
-MinimumScalarProduct.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
+Benchmark.bm do |bm|
+  bm.report("OnePass     ") do
+    NUMBER_OF_ITERATIONS.times do
+      MSP_OnePass.new([1,3,-5], [-2,4,1]).run
+    end
+  end
+  bm.report("Permutations") do
+    NUMBER_OF_ITERATIONS.times do
+      MinimumScalarProduct.new([1,3,-5], [-2,4,1]).run
+    end
+  end
 
-MSP_OnePass.new([0, -6, 3], [4, 1, 8]).run
-MinimumScalarProduct.new([0, -6, 3], [4, 1, 8]).run
+  bm.report("OnePass     ") do
+    NUMBER_OF_ITERATIONS.times do
+      MSP_OnePass.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
+    end
+  end
+  bm.report("Permutations") do
+    NUMBER_OF_ITERATIONS.times do
+      MinimumScalarProduct.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
+    end
+  end
 
+  bm.report("OnePass     ") do
+    NUMBER_OF_ITERATIONS.times do
+      MSP_OnePass.new([4, 9, -12, 18, 3, 37, 1, 4, 11, -18], [-1, 0, 2, 19, 44, 18, 0, 28, 43, 1]).run
+    end
+  end
+  bm.report("Permutations") do
+    NUMBER_OF_ITERATIONS.times do
+      MinimumScalarProduct.new([4, 9, -12, 18, 3, 37, 1, 4, 11, -18], [-1, 0, 2, 19, 44, 18, 0, 28, 43, 1]).run
+    end
+  end
+end
 
+MSP_OnePass.new([1,3,-5], [-2,4,1]).run.print_results
+MinimumScalarProduct.new([1,3,-5], [-2,4,1]).run.print_results
 
-# MinimumScalarProduct.new([1,3,-5], [-2,4,1]).run
-# MinimumScalarProduct.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
+MSP_OnePass.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run.print_results
+MinimumScalarProduct.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run.print_results
+
+MSP_OnePass.new([0, -6, 3], [4, 1, 8]).run.print_results
+MinimumScalarProduct.new([0, -6, 3], [4, 1, 8]).run.print_results
+
+MSP_OnePass.new([4, 9, -12, 18, 3, 37, 1, 4, 11, -18], [-1, 0, 2, 19, 44, 18, 0, 28, 43, 1]).run.print_results
+MinimumScalarProduct.new([4, 9, -12, 18, 3, 37, 1, 4, 11, -18], [-1, 0, 2, 19, 44, 18, 0, 28, 43, 1]).run.print_results
