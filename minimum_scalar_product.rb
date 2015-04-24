7b7c2403f1459db300402e1039bbc9d0a475e84a
John Moons <j.moons@realdigitalmedia.com>
2015-04-24 16:41:33 -0400

    adding a OnePass class for benchmarking

--- minimum_scalar_product.rb
+++ minimum_scalar_product.rb
@@ -1,3 +1,27 @@
+class MSP_OnePass
+  def initialize(vector1, vector2)
+    @vector1      = vector1
+    @vector2      = vector2
+    @smallest_sum = nil
+  end
+
+  def run
+    vector1_sorted          = @vector1.sort
+    vector2_sorted_reversed = @vector2.sort.reverse
+    calculate_sum_product(vector1_sorted, vector2_sorted_reversed)
+    puts "DONE (ONE PASS): #{@smallest_sum}"
+  end
+
+  private
+  def calculate_sum_product(vector1, vector2)
+    sum = 0
+    (0 ... vector1.length).each do |current_index|
+      sum += (vector1[current_index] * vector2[current_index])
+    end
+    @smallest_sum = sum
+  end
+end
+
 class MinimumScalarProduct
   def initialize(vector1, vector2)
     @vector1      = vector1
@@ -9,8 +33,8 @@ class MinimumScalarProduct
     @vector1.permutation.each do |vector1_permutation|
       test_permutation(vector1_permutation)
     end
-    puts "DONE: #{@smallest_sum.inspect}"
-    puts "DONE: #{@largest_sum.inspect}"
+    puts "DONE (Small): #{@smallest_sum.inspect}"
+    puts "DONE (Large): #{@largest_sum.inspect}"
   end
 
   private
@@ -35,5 +59,16 @@ class MinimumScalarProduct
   end
 end
 
+MSP_OnePass.new([1,3,-5], [-2,4,1]).run
 MinimumScalarProduct.new([1,3,-5], [-2,4,1]).run
+
+MSP_OnePass.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
 MinimumScalarProduct.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
+
+MSP_OnePass.new([0, -6, 3], [4, 1, 8]).run
+MinimumScalarProduct.new([0, -6, 3], [4, 1, 8]).run
+
+
+
+# MinimumScalarProduct.new([1,3,-5], [-2,4,1]).run
+# MinimumScalarProduct.new([1, 2, 3, 4, 5], [1, 0, 1, 0, 1]).run
